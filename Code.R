43a345f53d4abd76ecae312631836d0c2116b1c5
Thierry Schmidt <thierrypablo.schmidt@students.bfh.ch>
2019-05-03 13:26:51 +0200

    add cleanup code

--- Code.R
+++ Code.R
@@ -1,5 +1,37 @@
 library(dplyr)
-
+suppressMessages(library(dplyr))
 
 data <- read.csv("KaggleV2-May-2016.csv")
 data <- tbl_df(data)
+
+#####
+# Data Cleanup
+#####
+
+### Clean up Types
+# to factor
+data$Scholarship <- as.factor(data$Scholarship)
+data$Hipertension <- as.factor(data$Hipertension)
+data$Diabetes <- as.factor(data$Diabetes)
+data$Alcoholism <- as.factor(data$Alcoholism)
+data$Handcap <- as.factor(data$Handcap)
+data$SMS_received <- as.factor(data$SMS_received)
+
+# to Date factor, --> YYYY-MM-DD
+data$ScheduledDay <- as.Date.factor(data$ScheduledDay)
+data$AppointmentDay <- as.Date.factor(data$AppointmentDay)
+
+
+### Clean up variable Age
+range(data$Age)
+
+# patients with error in age
+ageErr <- filter(data, data$Age < 0)
+ageErr
+
+# start at age 0
+data <- filter(data, data$Age >= 0)
+range(data$Age)
+
+
+
